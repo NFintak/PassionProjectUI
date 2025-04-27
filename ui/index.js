b44aeef1338e3305f8230906e46921f699fb8498
Nathan <natefintak@gmail.com>
2025-04-27 17:49:39 -0400

    starting

---
+++ ui/index.js
@@ -0,0 +1,42 @@
+const API_URL = 'http://localhost:8080';
+
+function fetchLevelSelection() {
+    fetch(`${API_URL}/api/index`)
+    .then(res => {
+    return res.json();
+    })
+    .then(data => {
+        console.log(`data ${data}`);
+        show(data);
+    })
+    .catch(error => {
+    errm = `Error fetching data : ${error}`
+    console.log(`Error fetching data: ${errm}`);
+    console.error("CORS err", errm)
+    document.getElementById('level').innerHTML = errm;
+    });
+}
+
+function show(data) {
+
+    const ul = document.getElementById('level');
+    const list = document.createDocumentFragment();
+
+    data.map(function (post) {
+        console.log('Levels:', post);
+        let li = document.createElement('li');
+        let name = document.createElement('p');
+        let desc = document.createElement('p');
+        name.innerHTML = `<a href="./location.html?id=1"> ${post.name}</a>`;
+        desc.innerHTML = `${post.desc}`;
+
+        li.appendChild(name);
+        li.appendChild(desc);
+        list.appendChild(li);
+
+        });
+
+    ul.appendChild(list);
+}
+
+fetchLevelSelection();
---
+++ ui/level_selection.js
@@ -0,0 +1,42 @@
+const API_URL = 'http://localhost:8080';
+
+function fetchLevelSelection() {
+    fetch(`${API_URL}/level_selection`)
+    .then(res => {
+    return res.json();
+    })
+    .then(data => {
+        console.log(`data ${data}`);
+        show(data);
+    })
+    .catch(error => {
+    errm = `Error fetching data : ${error}`
+    console.log(`Error fetching data: ${errm}`);
+    console.error("CORS err", errm)
+    document.getElementById('level').innerHTML = errm;
+    });
+}
+alert("Hello");
+function show(data) {
+
+    const ul = document.getElementById('posts');
+    const list = document.createDocumentFragment();
+
+    data.map(function (post) {
+        console.log('Levels:', post.desc);
+        let li = document.createElement('li');
+        let levelName = document.createElement('p');
+        let desc = document.createElement('p');
+        levelName.innerHTML = `<a href="./location.html?id=1"> ${post.levelName}</a>`;
+        desc.innerHTML = `${post.desc}`;
+
+        li.appendChild(levelName);
+        li.appendChild(desc);
+        list.appendChild(li);
+
+        });
+
+    ul.appendChild(list);
+}
+
+fetchLevelSelection();
